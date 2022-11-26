453fbdf13265a1f08887357711161c8afef06b93
AndriiChemerys <andriichemerys@gmail.com>
2022-11-26 12:10:35 +0100

    Creating Hero Banner and Nav

--- react_projects/1_modern_react_18_fitness_exercises_app_with_apis/fitness_exercises_app/src/components/Navbar.js
+++ react_projects/1_modern_react_18_fitness_exercises_app_with_apis/fitness_exercises_app/src/components/Navbar.js
@@ -9,7 +9,12 @@ const Navbar = () => {
     <Stack
       direction="row"
       justifyContent="space-around"
-      sx={{ gap: { sm: "122px", xs: "40px" }, mt: { sm: "32", xs: "20px" } }}
+      sx={{
+        gap: { sm: "123px", xs: "40px" },
+        mt: { sm: "32px", xs: "20px" },
+        justifyContent: "none",
+      }}
+      px="20px"
     >
       <Link to="/">
         <img
@@ -22,7 +27,7 @@ const Navbar = () => {
           }}
         />
       </Link>
-      <Stack direction="row" gap="40px" font-size="24px" alignItems="flex-end">
+      <Stack direction="row" gap="40px" fontSize="24px" alignItems="flex-end">
         <Link
           to="/"
           style={{ textDecoration: "none", color: "#3A1212", borderBottom: "3px solid #FF2625" }}
